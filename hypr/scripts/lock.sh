66845163f34b08d499afec7d92255914b7677e84
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-03-13 17:53:16 +0000

    init

---
+++ hypr/scripts/lock.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+swaylock \
+    --hide-keyboard-layout \
+    --ring-color 313244 \
+    --ring-ver-color cba6f7 \
+    --ring-wrong-color f38ba8 \
+    --ring-clear-color a6e3a1 \
+    --key-hl-color cba6f7 \
+    --bs-hl-color fab387 \
+    --text-color cdd6f4 \
+    --text-caps-lock-color f38ba8 \
+    --text-wrong-color cdd6f4 \
+    -r \
+    --separator-color 313244
+    --inside-color 313244 \
+    --inside-ver-color 89b4fa\
+    --inside-wrong-color f38ba8 \
+    --inside-clear-color a6e3a1 \
+    --clock \
+    --indicator \
+    --grace-no-mouse \
+    --grace-no-touch \
+    --image ~/.config/hypr/wallpapers/landscape-extended.png \
