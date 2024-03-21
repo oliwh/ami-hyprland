b27cdab428893d982013312b2bd21a0d3cca8f5d
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-03-21 22:23:32 +0000

    started customising waybar - BIOS SETUP UTILITY TEXT + battery on topbar - nothing on bottom bar

--- hypr/scripts/on-start.sh
+++ hypr/scripts/on-start.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 swaybg --output '*' --color "#aaaaaa" &
-waybar &
+cd ~/.config/waybar/topbar
+waybar -c config.jsonc -s style.css &
+sleep 0.05
+cd ~/.config/waybar/mainbar
+waybar -c config.jsonc -s style.css &
+cd ~/.config/waybar/bottombar
+
+
