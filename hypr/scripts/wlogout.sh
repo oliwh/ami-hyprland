cde8107f5fbb986a2cec53aa58e28faeda40c779
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-03-22 12:11:33 +0000

    fixed wlogout menu alignment for different resolutions

--- hypr/scripts/wlogout.sh
+++ hypr/scripts/wlogout.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-wlogout -b 1 -T 600em -B "2000em" -L 940em -R 940em
+width=$(hyprctl monitors | sed -n '2 p'| sed 's/@.*//' | sed 's/[x]/ /g' | awk '{print $1}')
+height=$(hyprctl monitors | sed -n '2 p'| sed 's/@.*//' | sed 's/[x]/ /g' | awk '{print $2}')
+# wlogout menu width/4 ~= 213px, height/4 ~= 120px
+w=$((width/2 - 213))
+h=$((height/2 - 120))
+wlogout --protocol layer-shell -b 1 -T $h -B $(($h * 2)) -L $w -R $w
+
